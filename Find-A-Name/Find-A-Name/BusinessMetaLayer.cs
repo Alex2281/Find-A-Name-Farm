c87b6cc84978532269aa8b844a24163d78784b55
Alex Alexander <411558@edu.cwa.ac.uk>
2018-11-07 09:50:55 +0000

    jdjdfhjd

---
+++ Find-A-Name/Find-A-Name/BusinessMetaLayer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Find_A_Name
+{
+    public class BusinessMetaLayer
+    {
+        static private BusinessMetaLayer m_instance = null;
+
+        private BusinessMetaLayer() { }
+
+        static public BusinessMetaLayer instance()
+        {
+            if (null == m_instance)
+            {
+                m_instance = new BusinessMetaLayer();
+            }
+            return m_instance;
+        }
+
+        // Could just have a set of static helper methods rather than a singleton!
+        public List<Employee> getEmployees()
+        {
+            List<Employee> employees = new List<Employee>();
+
+            DbConection con = DbFactory.instance();
+            if (con.OpenConnection())
+            {
+                DbDataReader dr = con.Select("SELECT CUST_ID, cust_name, cust_address, cust_city FROM customers;");
+
+                //Read the data and store them in the list
+                while (dr.Read())
+                {
+                    Employee employee = new Employee();
+                    employee.ID = dr.GetInt32(0);
+                    employee.Name = dr.GetString(1);
+                    employee.Address = dr.GetString(2);
+                    employee.City = dr.GetString(3);
+                    // etc.....
+
+                    employees.Add(employee);
+                }
+
+                //close Data Reader
+                dr.Close();
+                con.CloseConnection();
+            }
+
+            return employees;
+        }
+    }
+}
