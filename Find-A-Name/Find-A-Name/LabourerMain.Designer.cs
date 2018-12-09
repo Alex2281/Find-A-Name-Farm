d7f972add70a482c9288b44bc20f5a44f237880b
Alex Alexander <411558@edu.cwa.ac.uk>
2018-12-09 00:53:30 +0000

    labourer main update

--- Find-A-Name/Find-A-Name/LabourerMain.Designer.cs
+++ Find-A-Name/Find-A-Name/LabourerMain.Designer.cs
@@ -42,19 +42,23 @@
             this.lblAddress = new System.Windows.Forms.Label();
             this.lblName = new System.Windows.Forms.Label();
             this.dgdUserTasks = new System.Windows.Forms.DataGridView();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.gbTaskViewer = new System.Windows.Forms.GroupBox();
             this.btnLogOut = new System.Windows.Forms.Button();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.gbJobStatus = new System.Windows.Forms.GroupBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.lbvPending = new System.Windows.Forms.Label();
             this.lbvComplete = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.lbv4 = new System.Windows.Forms.Label();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnPrevious = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
             this.gbUserDetails.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgdUserTasks)).BeginInit();
-            this.groupBox2.SuspendLayout();
+            this.gbTaskViewer.SuspendLayout();
+            this.gbJobStatus.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -102,7 +106,7 @@
             // 
             // btnChangeDetails
             // 
-            this.btnChangeDetails.Location = new System.Drawing.Point(182, 198);
+            this.btnChangeDetails.Location = new System.Drawing.Point(187, 202);
             this.btnChangeDetails.Name = "btnChangeDetails";
             this.btnChangeDetails.Size = new System.Drawing.Size(136, 35);
             this.btnChangeDetails.TabIndex = 10;
@@ -148,7 +152,7 @@
             // lblEmailAddress
             // 
             this.lblEmailAddress.AutoSize = true;
-            this.lblEmailAddress.Location = new System.Drawing.Point(22, 136);
+            this.lblEmailAddress.Location = new System.Drawing.Point(16, 136);
             this.lblEmailAddress.Name = "lblEmailAddress";
             this.lblEmailAddress.Size = new System.Drawing.Size(102, 17);
             this.lblEmailAddress.TabIndex = 5;
@@ -157,7 +161,7 @@
             // lblPhoneNumber
             // 
             this.lblPhoneNumber.AutoSize = true;
-            this.lblPhoneNumber.Location = new System.Drawing.Point(22, 101);
+            this.lblPhoneNumber.Location = new System.Drawing.Point(16, 101);
             this.lblPhoneNumber.Name = "lblPhoneNumber";
             this.lblPhoneNumber.Size = new System.Drawing.Size(107, 17);
             this.lblPhoneNumber.TabIndex = 4;
@@ -166,7 +170,7 @@
             // lblAddress
             // 
             this.lblAddress.AutoSize = true;
-            this.lblAddress.Location = new System.Drawing.Point(21, 68);
+            this.lblAddress.Location = new System.Drawing.Point(15, 68);
             this.lblAddress.Name = "lblAddress";
             this.lblAddress.Size = new System.Drawing.Size(64, 17);
             this.lblAddress.TabIndex = 3;
@@ -175,7 +179,7 @@
             // lblName
             // 
             this.lblName.AutoSize = true;
-            this.lblName.Location = new System.Drawing.Point(21, 36);
+            this.lblName.Location = new System.Drawing.Point(15, 36);
             this.lblName.Name = "lblName";
             this.lblName.Size = new System.Drawing.Size(49, 17);
             this.lblName.TabIndex = 2;
@@ -192,45 +196,48 @@
             this.dgdUserTasks.Size = new System.Drawing.Size(467, 166);
             this.dgdUserTasks.TabIndex = 2;
             // 
-            // groupBox1
+            // gbTaskViewer
             // 
-            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox1.Location = new System.Drawing.Point(12, 307);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(838, 241);
-            this.groupBox1.TabIndex = 3;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "View Task";
+            this.gbTaskViewer.Controls.Add(this.btnNext);
+            this.gbTaskViewer.Controls.Add(this.btnPrevious);
+            this.gbTaskViewer.Controls.Add(this.btnUpdate);
+            this.gbTaskViewer.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbTaskViewer.Location = new System.Drawing.Point(12, 307);
+            this.gbTaskViewer.Name = "gbTaskViewer";
+            this.gbTaskViewer.Size = new System.Drawing.Size(838, 241);
+            this.gbTaskViewer.TabIndex = 3;
+            this.gbTaskViewer.TabStop = false;
+            this.gbTaskViewer.Text = "View Task";
             // 
             // btnLogOut
             // 
-            this.btnLogOut.Location = new System.Drawing.Point(21, 198);
+            this.btnLogOut.Location = new System.Drawing.Point(16, 202);
             this.btnLogOut.Name = "btnLogOut";
             this.btnLogOut.Size = new System.Drawing.Size(136, 35);
             this.btnLogOut.TabIndex = 11;
             this.btnLogOut.Text = "Log Out";
             this.btnLogOut.UseVisualStyleBackColor = true;
             // 
-            // groupBox2
+            // gbJobStatus
             // 
-            this.groupBox2.Controls.Add(this.lbv4);
-            this.groupBox2.Controls.Add(this.label3);
-            this.groupBox2.Controls.Add(this.lbvComplete);
-            this.groupBox2.Controls.Add(this.lbvPending);
-            this.groupBox2.Controls.Add(this.label2);
-            this.groupBox2.Controls.Add(this.label1);
-            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox2.Location = new System.Drawing.Point(378, 227);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(472, 65);
-            this.groupBox2.TabIndex = 4;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Job Status";
+            this.gbJobStatus.Controls.Add(this.lbv4);
+            this.gbJobStatus.Controls.Add(this.label3);
+            this.gbJobStatus.Controls.Add(this.lbvComplete);
+            this.gbJobStatus.Controls.Add(this.lbvPending);
+            this.gbJobStatus.Controls.Add(this.label2);
+            this.gbJobStatus.Controls.Add(this.label1);
+            this.gbJobStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbJobStatus.Location = new System.Drawing.Point(378, 227);
+            this.gbJobStatus.Name = "gbJobStatus";
+            this.gbJobStatus.Size = new System.Drawing.Size(472, 65);
+            this.gbJobStatus.TabIndex = 4;
+            this.gbJobStatus.TabStop = false;
+            this.gbJobStatus.Text = "Job Status";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 36);
+            this.label1.Location = new System.Drawing.Point(40, 36);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(64, 17);
             this.label1.TabIndex = 0;
@@ -239,7 +246,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(148, 36);
+            this.label2.Location = new System.Drawing.Point(175, 36);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(79, 17);
             this.label2.TabIndex = 1;
@@ -249,7 +256,7 @@
             // 
             this.lbvPending.AutoSize = true;
             this.lbvPending.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbvPending.Location = new System.Drawing.Point(77, 14);
+            this.lbvPending.Location = new System.Drawing.Point(104, 14);
             this.lbvPending.Name = "lbvPending";
             this.lbvPending.Size = new System.Drawing.Size(43, 46);
             this.lbvPending.TabIndex = 2;
@@ -259,7 +266,7 @@
             // 
             this.lbvComplete.AutoSize = true;
             this.lbvComplete.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbvComplete.Location = new System.Drawing.Point(225, 16);
+            this.lbvComplete.Location = new System.Drawing.Point(252, 16);
             this.lbvComplete.Name = "lbvComplete";
             this.lbvComplete.Size = new System.Drawing.Size(43, 46);
             this.lbvComplete.TabIndex = 0;
@@ -268,7 +275,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(315, 35);
+            this.label3.Location = new System.Drawing.Point(342, 35);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(38, 17);
             this.label3.TabIndex = 3;
@@ -278,11 +285,38 @@
             // 
             this.lbv4.AutoSize = true;
             this.lbv4.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbv4.Location = new System.Drawing.Point(372, 16);
+            this.lbv4.Location = new System.Drawing.Point(399, 16);
             this.lbv4.Name = "lbv4";
             this.lbv4.Size = new System.Drawing.Size(43, 46);
             this.lbv4.TabIndex = 4;
             this.lbv4.Text = "3";
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(689, 195);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(136, 35);
+            this.btnUpdate.TabIndex = 0;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // btnPrevious
+            // 
+            this.btnPrevious.Location = new System.Drawing.Point(6, 200);
+            this.btnPrevious.Name = "btnPrevious";
+            this.btnPrevious.Size = new System.Drawing.Size(120, 35);
+            this.btnPrevious.TabIndex = 1;
+            this.btnPrevious.Text = "<<Previous";
+            this.btnPrevious.UseVisualStyleBackColor = true;
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(132, 200);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(120, 35);
+            this.btnNext.TabIndex = 2;
+            this.btnNext.Text = "Next >>";
+            this.btnNext.UseVisualStyleBackColor = true;
             // 
             // LabourerMain
             // 
@@ -290,8 +324,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(864, 560);
-            this.Controls.Add(this.groupBox2);
-            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.gbJobStatus);
+            this.Controls.Add(this.gbTaskViewer);
             this.Controls.Add(this.dgdUserTasks);
             this.Controls.Add(this.gbUserDetails);
             this.Controls.Add(this.menuStrip1);
@@ -305,8 +339,9 @@
             this.gbUserDetails.ResumeLayout(false);
             this.gbUserDetails.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgdUserTasks)).EndInit();
-            this.groupBox2.ResumeLayout(false);
-            this.groupBox2.PerformLayout();
+            this.gbTaskViewer.ResumeLayout(false);
+            this.gbJobStatus.ResumeLayout(false);
+            this.gbJobStatus.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -328,14 +363,17 @@
         private System.Windows.Forms.Label lbvAddress;
         private System.Windows.Forms.Label lbvName;
         private System.Windows.Forms.Button btnChangeDetails;
-        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox gbTaskViewer;
         private System.Windows.Forms.Button btnLogOut;
-        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox gbJobStatus;
         private System.Windows.Forms.Label lbv4;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label lbvComplete;
         private System.Windows.Forms.Label lbvPending;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnPrevious;
+        private System.Windows.Forms.Button btnUpdate;
     }
 }
